3e1f0ef61a65c30b8dffcfedadb3afb17ef8800c
Floinvite Dev <dev@floinvite.com>
2025-12-31 21:03:16 +0000

    Allow custom email recipients in send

--- public/floinvite-mail/login.php
+++ public/floinvite-mail/login.php
@@ -1,5 +1,8 @@
 <?php
-require_once 'config.php';
+require_once 'logo.php';
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = $_POST['password'] ?? '';
