6c08a1ac3c435e9ddb44b36ce6a6a21cc2cbfa3b
Floinvite Dev <dev@floinvite.com>
2025-12-31 19:10:18 +0000

    Log floinvite-mail PHP errors

--- public/floinvite-mail/config.php
+++ public/floinvite-mail/config.php
@@ -7,6 +7,8 @@
 // Error Reporting
 error_reporting(E_ALL);
 ini_set('display_errors', 0);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/logs/error_log');
 
 // Load environment variables
 require_once dirname(dirname(__DIR__)) . '/public/api/env.php';
